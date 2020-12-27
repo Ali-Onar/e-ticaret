af442462fe19bb69020cc0585b439cdcd330bb9f
= <alitunacanonar@icloud.com>
2020-12-27 23:49:12 +0300

    listing the products added to the cart

--- nedmin/netting/sdf.php
+++ nedmin/netting/sdf.php
@@ -1,18 +1,5 @@
-$sepetkaydet = $db->prepare("INSERT INTO sepetlar SET 
-    kullanici_id =:kullanici_id,
-    urun_id =:urun_id,
-    sepet_detay=:sepet_detay
-    ");
-
-    $insert = $sepetkaydet->execute(array(
-        'kullanici_id' => $_POST['kullanici_id'],
-        'urun_id' => $_POST['urun_id'],
-        'sepet_detay' => $_POST['sepet_detay']
-    ));
-
-    if ($insert) {
-        header("location:$gelen_url?durum=ok");
-    } else {
-        header("location:$gelen_url?durum=no");
-    }
-}
+$sepetsor = $db->prepare("SELECT * FROM sepet where sepet_id=:id");
+$sepetsor->execute(array(
+  'id' => $_GET['sepet_id']
+));
+$sepetcek = $sepetsor->fetch(PDO::FETCH_ASSOC);
