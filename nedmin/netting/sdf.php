93699ccdde46fc801f51946056dfacad507b6348
= <alitunacanonar@icloud.com>
2021-01-01 17:49:03 +0300

    Image editing in featured products

--- nedmin/netting/sdf.php
+++ nedmin/netting/sdf.php
@@ -1,20 +1,2 @@
-//yorum SİLME
-
-if ($_GET['yorumsil'] == "ok") {
-
-    $sil = $db->prepare("DELETE from yorum where yorum_id=:yorum_id");
-    $kontrol = $sil->execute(array(
-        'yorum_id' => $_GET['yorum_id']
-    ));
-
-    if ($kontrol) {
-
-        $resimsilunlink = $_GET['yorum_resimyol'];
-        unlink("../../$resimsilunlink");
-
-        Header("Location:../production/yorum.php?durum=ok");
-    } else {
-
-        Header("Location:../production/yorum.php?durum=no");
-    }
-}
+$urunfotosor = $db->prepare("SELECT * FROM urunfoto order by urunfoto_id DESC");
+$urunfotosor->execute();
